09469ac04159bed6ae54a2d8806500dda2957bf1
Kozyavin V <kioci@yandex.ru>
2023-04-08 11:25:10 +0300

    InitialCommit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int account = 100; // У клиента денег на счету
+        int payment = 1999; // Платёж клиента на личный счёт. Пополнение счёта.
+        int bonusPay = payment/100; //За каждые 100 руб. суммы пополнения = 1 бонус
+        if (payment > 1000) {System.out.println("На вашем счёте: "+(account+payment+bonusPay)+" рубль(ей)\n" +
+                "Бонусных - "+bonusPay+" рублей ");}
+        else {System.out.println("На вашем счёте: "+(account+payment)+" рублей.\n" +
+                "Условие для начисления бонусов не выполнено.");};
+    }
+}
