fd502300b0f2190a5873dfd6af9d49dd9d54f5a4
Kozyavin V <kioci@yandex.ru>
2023-04-08 11:36:47 +0300

    Changes

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int account = 100; // У клиента денег на счету
-        int payment = 1999; // Платёж клиента на личный счёт. Пополнение счёта.
+        int payment = 1100; // Платёж клиента на личный счёт. Пополнение счёта.
         int bonusPay = payment/100; //За каждые 100 руб. суммы пополнения = 1 бонус
         if (payment > 1000) {System.out.println("На вашем счёте: "+(account+payment+bonusPay)+" рубль(ей)\n" +
                 "Бонусных - "+bonusPay+" рублей ");}
