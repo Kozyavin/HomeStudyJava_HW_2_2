f83c5449f6086fb99aad50708d298d9d6204f6f0
Kozyavin V <kioci@yandex.ru>
2023-04-08 20:53:10 +0300

    CodeReformat

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
 
         int account = 100; // У клиента денег на счету
         int payment = 1100; // Платёж клиента на личный счёт. Пополнение счёта.
-        int bonusPay = payment/100; //За каждые 100 руб. суммы пополнения = 1 бонус
-        if (payment > 1000) {System.out.println("На вашем счёте: "+(account+payment+bonusPay)+" рубль(ей)\n" +
-                "Бонусных - "+bonusPay+" рублей ");}
-        else {System.out.println("На вашем счёте: "+(account+payment)+" рублей.\n" +
-                "Условие для начисления бонусов не выполнено.");};
+        int bonusPay = payment / 100; //За каждые 100 руб. суммы пополнения = 1 бонус
+        if (payment > 1000) {
+            System.out.println("На вашем счёте: " + (account + payment + bonusPay) + " рубль(ей)\n" + "Бонусных - " + bonusPay + " рублей ");
+        } else {
+            System.out.println("На вашем счёте: " + (account + payment) + " рублей.\n" + "Условие для начисления бонусов не выполнено.");
+        }
+        ;
     }
 }
